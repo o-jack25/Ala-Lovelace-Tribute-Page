8944d4e7228e9fdb6197c02e7df9e39bbac7adc4
Jack <106848993+o-jack25@users.noreply.github.com>
2023-07-17 16:22:34 -0400

    Add files via upload

---
+++ tribute.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const divider = document.getElementById('divider');
+    const h2Width = document.querySelector('.home h2').offsetWidth;
+    divider.style.width = `${h2Width}px`;
+  });
+  
